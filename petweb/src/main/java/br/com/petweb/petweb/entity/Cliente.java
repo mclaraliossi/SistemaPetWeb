ef6f6320a026e71e1bf2a7e8a7f5bcdf6fdfc69f
Maria Liossi <mariaclaraliossi@gmail.com>
2026-02-25 14:53:16 -0300

    Fazendo a classe e configurando o banco de dados

--- petweb/src/main/java/br/com/petweb/petweb/entity/Cliente.java
+++ petweb/src/main/java/br/com/petweb/petweb/entity/Cliente.java
@@ -1,11 +1,42 @@
 package br.com.petweb.petweb.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Table(name = "cliente")
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "cliente") /*define o nnome da tabela dentro do banco de dados */
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer idCliente;
+
+    @Column(nullable = false, length = 40)
+    private String nomeCliente;
+
+    @Column(nullable = false, length = 100)
+    private String emailCliente;
+
+    @Column(nullable = false, length = 15)
+    private String telefonecliente;
+
+    @Column(nullable = false, length = 50)
+    private String enderecoCliente;
+
+    @Column(nullable = false, length = 14)
+    private String cpfCliente;
+
     
 }
