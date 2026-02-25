4861d19f74e17c7aff6d408ada7ff3e4a2048565
Maria Liossi <mariaclaraliossi@gmail.com>
2026-02-25 14:37:54 -0300

    Subindo para o repositório

---
+++ petweb/src/main/java/br/com/petweb/petweb/PetwebApplication.java
@@ -0,0 +1,13 @@
+package br.com.petweb.petweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetwebApplication.class, args);
+	}
+
+}
---
+++ petweb/src/test/java/br/com/petweb/petweb/PetwebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petweb.petweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
